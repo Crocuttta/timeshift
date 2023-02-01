8b96a2e33e64bc4cd6cde1a9cae74a2515b34586
Ольга Казанцева <masqcat@gmail.com>
2023-02-01 14:23:22 +0300

    Комплексные изменения: - объединение классов в timeshift_lib - вывод локального времени города клиенту

--- web/timeshift/timeshift_lib.py
+++ web/timeshift/timeshift_lib.py
@@ -2,12 +2,17 @@ import datetime
 
 import requests
 
-class Provider:
 
-    def show_cities_list(self, cities):
-        for city_name in sorted(cities):
-            local_time = self.get_local_time(cities[city_name])
-            print(f'{city_name}: {local_time}')
+
+
+class Abstractapi():
+    
+    ABSTRACT_API_URL = (
+        'https://timezone.abstractapi.com/v1/current_time/?'
+        'api_key=67a70fd196da44f19184fefc7f80145f&location='
+    )
+
+    
 
 
     def get_local_time(
@@ -26,23 +31,7 @@ class Provider:
             current_utc_time
             + datetime.timedelta(hours=gmt_offset)
         ).strftime('%H:%M')    
-
-
-class Abstractapi(Provider):
     
-    ABSTRACT_API_URL = (
-        'https://timezone.abstractapi.com/v1/current_time/?'
-        'api_key=67a70fd196da44f19184fefc7f80145f&location='
-    )
-
-    @classmethod
-    def class_method(cls):        
-        pass
-
-
-    @staticmethod
-    def static_method():
-        pass    
 
 
     def find_city(self, city_name):
@@ -53,10 +42,17 @@ class Abstractapi(Provider):
         city_data = requests.get(self.ABSTRACT_API_URL + city_name).json()
         print('>>>> ', city_data)
         if not city_data or city_data.get('error', False):
-            return None       
+            return {
+                'error' : f'Город с именем "{city_name}" не найден.'
+            }      
         print('Текущее время: ', self.get_local_time(city_data['gmt_offset']))
         return {
             'gmt_offset' : city_data['gmt_offset'],
             'name' : city_name
-        }    
+        } 
+
+    def show_cities_list(self, cities):
+        for city_name in sorted(cities):
+            local_time = self.get_local_time(cities[city_name])
+            print(f'{city_name}: {local_time}')       
     
